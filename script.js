0a7f9c711428e71c63a49d7de93e61d0f76e1659
marcel fuller <Marcel.fuller@outlook.com>
2021-12-01 11:52:21 -0600

    adding files
    adding files

---
+++ script.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+var time = d.getHours();
+var greeting= "hello"
+function myfunc(){
+	var msg="dont forget to check out the new appetizers!";
+	alert(greeting + msg);
+}
+
+if (time < 12) {
+	document.write("<b>good morning</b>");
+	 }
+
+if (time>=12){document.write("<b>Lunch and dinner specials</b>");
+		}
+let question = prompt("are you over the age of 18?");
+
+let a= 5;
+let b= 15;
+console.log("the value of a is " + a );
+				   
+	let city= "San Antonio";
+	let state= "Texas";
+	let home= city + "" + state;
+ 
+	
+	//conditional statements
+var allowRegister = {age < 18 ? true : false;}
+
+if (allowRegister){alert("You may not enter unless you're 18");}
+else (allowRegister) {alert("You may enter");}
+
+document.getElementById("demo").innerHTML = "my restraunt"; }
+
+document.write(5 + 6); window.alert(5 + 6);  console.log(5 + 6); 
